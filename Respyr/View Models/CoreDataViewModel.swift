5408928f8b3ab13f82406bdd101a18e80d3d743b
kevinjparke <kjparke@gmail.com>
2021-10-22 21:56:08 -0400

    Added helpers to update view context

--- Respyr/View Models/CoreDataViewModel.swift
+++ Respyr/View Models/CoreDataViewModel.swift
@@ -40,6 +40,7 @@ class CoreDataViewModel: ObservableObject {
     }
     
     func addUser(user: User) {
+        //Create a user
         let newUser = UserAccount(context: container.viewContext)
         newUser.userID = user.id
         newUser.fullName = user.fullName
@@ -54,4 +55,78 @@ class CoreDataViewModel: ObservableObject {
             print("Error saving. \(error)")
         }
     }
+    
+    func addToTrainingCenter(_ id: String) {
+        let trainingCenter = TrainingCenterData(context: container.viewContext)
+        trainingCenter.id = id
+        
+        if !savedUserEntity.isEmpty {
+            savedUserEntity.first!.addToTrainingCenters(trainingCenter)
+        }
+        
+        self.saveData()
+    }
+    
+    func addToSessionID(_ id: String) {
+        let sessionID = SessionIDs(context: container.viewContext)
+        sessionID.id = id
+        
+        if !savedUserEntity.isEmpty {
+            savedUserEntity.first!.addToSessionIDs(sessionID)
+        }
+        
+        self.saveData()
+    }
+    
+    func addToSentRequests(_ id: String) {
+        let sentRequest = SentRequests(context: container.viewContext)
+        sentRequest.id = id
+        
+        if !savedUserEntity.isEmpty {
+            savedUserEntity.first!.addToSentRequest(sentRequest)
+        }
+        
+        self.saveData()
+    }
+    
+    func addToAdminTC(_ id: String) {
+        let adminTC = AdminTC(context: container.viewContext)
+        adminTC.id = id
+        
+        if !savedUserEntity.isEmpty {
+            savedUserEntity.first!.addToAdminTC(adminTC)
+        }
+        self.saveData()
+    }
+    
+    func addToInstructorTC(_ id: String){
+        let instructorTC = InstructorTC(context: container.viewContext)
+        instructorTC.id = id
+        
+        if !savedUserEntity.isEmpty {
+            savedUserEntity.first!.addToInstructorTC(instructorTC)
+        }
+        self.saveData()
+    }
+    
+    func addToSessionReservations(_ id: String) {
+        let sessionReservation = SessionReservations(context: container.viewContext)
+        sessionReservation.id = id
+        
+        if !savedUserEntity.isEmpty {
+            savedUserEntity.first!.addToSessionReservations(sessionReservation)
+        }
+        self.saveData()
+    }
+    
+    func addToChecklists(_ id: String) {
+        let checklists = Checklists(context: container.viewContext)
+        checklists.id = id
+        
+        if !savedUserEntity.isEmpty {
+            savedUserEntity.first!.addToChecklists(checklists)
+        }
+        self.saveData()
+    }
+    
 }
